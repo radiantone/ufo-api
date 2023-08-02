8a9e52f31186bbd0e9dbebd36bf13f8a3918b73d
Darren Govoni <darren@rapidquest.ai>
2023-08-02 17:59:45 -0400

    Update __init__.py

--- ufo/api/__init__.py
+++ ufo/api/__init__.py
@@ -81,45 +81,6 @@ def handle_auth_error(ex):
     return response
 
 
-def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header"""
-    auth = request.headers.get("Authorization", None)
-    if not auth:
-        raise AuthError(
-            {
-                "code": "authorization_header_missing",
-                "description": "Authorization header is expected",
-            },
-            401,
-        )
-
-    parts = auth.split()
-
-    if parts[0].lower() != "bearer":
-        raise AuthError(
-            {
-                "code": "invalid_header",
-                "description": "Authorization header must start with" " Bearer",
-            },
-            401,
-        )
-    elif len(parts) == 1:
-        raise AuthError(
-            {"code": "invalid_header", "description": "Token not found"}, 401
-        )
-    elif len(parts) > 2:
-        raise AuthError(
-            {
-                "code": "invalid_header",
-                "description": "Authorization header must be" " Bearer token",
-            },
-            401,
-        )
-
-    token = parts[1]
-    return token
-
-
 class SearchService(Resource):
     def get(self):
         return "Unimplemented"
